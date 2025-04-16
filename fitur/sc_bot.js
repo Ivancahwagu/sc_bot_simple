c6f3a5c46c0f6f7499e62820f03d14a9836a645e
Ivancahwagu <ivantheo159@gmail.com>
2025-04-16 13:57:46 +0700

    update

---
+++ fitur/sc_bot.js
@@ -0,0 +1,12 @@
+let theoFitur = async function ({ m, theo }) {
+    await m.reply(`👨‍💻 *Script Bot WhatsApp - ${namaBot}*
+
+📦 Source code tersedia di GitHub:
+🔗 https://github.com/Ivancahwagu/sc_bot_simple
+
+Silakan cek, clone, dan kembangkan sesuai kreativitasmu! Jangan lupa kasih ⭐ kalau suka 😎`)
+}
+
+theoFitur.command = ["sc", "script", "scbot"]
+theoFitur.tags = "main"
+export default theoFitur
